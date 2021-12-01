416c26d5ea0b09d9287f6e56af3d2ea9232df006
Antonio Berardi <adberardi@yahoo.com>
2021-12-01 19:06:45 -0400

    Clase 7: Eventos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,12 +2,30 @@ import React from "react";
 import "./TodoItem.css";
 
 function TodoItem(props) {
+  /* Si una funcion requiere de un parametro que se emplea en el Html, es necesario usar la arrow function */
+  const onComplete = (txt) => {
+    console.log(`Completando tarea ${txt}`);
+  };
+
+  /* Si una funcion no requiere de algun parametro, basta con emplear solo el objeto dentro del evento */
+  const onDelete = () => {
+    console.log(`Borrando tarea`);
+  };
+
   return (
-      <div className="TodoItem">
-        <input type="checkbox" id={props.text} value={props.text} />{" "}
-        <label htmlFor={props.text}> {props.text} </label>
-        <button className="btn-cancel" type="button"> X </button>
-      </div>
+    <div className="TodoItem">
+      <input
+        type="checkbox"
+        id={props.text}
+        value={props.text}
+        onClick={() => onComplete(props.text)}
+      />{" "}
+      <label htmlFor={props.text}> {props.text} </label>
+      <button className="btn-cancel" type="button" onClick={onDelete}>
+        {" "}
+        X{" "}
+      </button>
+    </div>
   );
 }
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,9 +2,14 @@ import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
+
+  const onSearchValueHandler = (event) => {
+    console.log(event.target.value);
+  }
+
   return (
     <div className="canvas">
-      <input type="text" className="inputSearch" placeholder="Indique parametro..." />
+      <input type="text" className="inputSearch" placeholder="Indique parametro..." onChange={onSearchValueHandler} />
     </div>
   );
 }
