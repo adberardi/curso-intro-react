0b58fde798fa10376d84bdbc5185ad63f53a9fde
Antonio Berardi <adberardi@yahoo.com>
2021-12-06 17:42:47 -0400

    Clase 8: Estados en React

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,15 +2,26 @@ import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
+  const [param, setParam] = React.useState("");
 
   const onSearchValueHandler = (event) => {
+    setParam(event.target.value);
     console.log(event.target.value);
-  }
+  };
 
   return (
-    <div className="canvas">
-      <input type="text" className="inputSearch" placeholder="Indique parametro..." onChange={onSearchValueHandler} />
-    </div>
+    <React.Fragment>
+      <div className="canvas">
+        <input
+          type="text"
+          className="inputSearch"
+          value={param}
+          placeholder="Indique parametro..."
+          onChange={onSearchValueHandler}
+        />
+      </div>
+      <p> {param} </p>
+    </React.Fragment>
   );
 }
 
