fc1b80befd39a3d91824a2dcd78a2934029b1799
Antonio Berardi <adberardi@yahoo.com>
2022-01-19 20:08:05 -0400

    Clase 13: Custom Hooks

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -13,27 +13,9 @@ function AppUI({
   status,
   setStatus,
   listTodos,
+  onComplete,
+  onDelete
 }) {
-  /* Si una funcion requiere de un parametro que se emplea en el Html, es necesario usar la arrow function */
-  const onComplete = (txt) => {
-    let listAux = status;
-    listAux = listAux.filter((index) => {
-      if (index.text === txt) {
-        index.completed = !index.completed;
-        console.log(`Listo ${index.text}`);
-      }
-      return index;
-    });
-    setStatus(listAux);
-  };
-
-
-  const onDelete = (txt) => {
-      let i = status.findIndex((index) => index.text === txt);
-      let listNew = [...status];
-      listNew.splice(i,1);
-      setStatus(listNew);
-  }
 
   return (
     <React.Fragment>
--- src/App/index.js
+++ src/App/index.js
@@ -4,28 +4,71 @@ import { AppUI } from "./AppUI";
 import { Layout } from "../Layout";
 // import "./App.css";
 
-const todos = [
-  { text: "Comprar cebolla", completed: false },
-  { text: "Curso Intro a React", completed: false },
-  { text: "Llorar con la llorona", completed: false },
-];
+// Custom Hooks
+function useLocalStorage(itemName, initialValue) {
+  const localStorageTodos  = localStorage.getItem(itemName);
+  let parsedTodos;
+
+  // Se verifica si ya existe informacion almacenada, sino se crea.
+  if (!localStorageTodos) {
+    localStorage.setItem(itemName,JSON.stringify(initialValue));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [item, setItem] = React.useState(parsedTodos);
+
+  const saveTodos = (newArr) => {
+    localStorage.setItem(itemName,JSON.stringify(newArr));
+    setItem(newArr);
+  };
+
+  return [
+    item,
+    saveTodos
+  ];
+}
 
 function App() {
-  const [task, setTask] = React.useState(todos);
+  const [todos, setTodos] = useLocalStorage('TODOS_V1',[]);
+
   const [search, setSearch] = React.useState("");
 
-  let completed = task.filter((index) => index.completed === true);
+  let completed = todos.filter((index) => index.completed === true);
 
   let listTodos = [];
 
   if (search.length >= 1) {
-    listTodos = task.filter((index) => {
+    listTodos = todos.filter((index) => {
       const txt = index.text;
       const txtLower = txt.toLowerCase();
       return txtLower.includes(search);
     });
   } else {
-    listTodos = task;
+    listTodos = todos;
+  }
+
+
+  /* Si una funcion requiere de un parametro que se emplea en el Html, es necesario usar la arrow function */
+  const onCompleteTask = (txt) => {
+    let listAux = todos;
+    listAux = listAux.filter((index) => {
+      if (index.text === txt) {
+        index.completed = !index.completed;
+        console.log(`Listo ${index.text}`);
+      }
+      return index;
+    });
+    setTodos(listAux);
+  };
+
+
+  const onDeleteTask = (txt) => {
+      let i = todos.findIndex((index) => index.text === txt);
+      let listNew = [...todos];
+      listNew.splice(i,1);
+      setTodos(listNew);
   }
 
 
@@ -35,12 +78,14 @@ function App() {
       <Layout>
         <AppUI
           completed={completed.length}
-          total={task.length}
+          total={todos.length}
           param={search}
           setParam={setSearch}
-          status={task}
-          setStatus={setTask}
+          status={todos}
+          setStatus={setTodos}
           listTodos={listTodos}
+          onComplete={onCompleteTask}
+          onDelete={onDeleteTask}
         />
         
       </Layout>
