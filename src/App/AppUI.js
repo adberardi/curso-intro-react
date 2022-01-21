7066341d187bc9d4d5a1aa5d2a415640e710a2ba
Antonio Berardi <adberardi@yahoo.com>
2022-01-20 22:31:42 -0400

    Clase 15: Manejo de Context

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,49 +1,62 @@
 import React from "react";
+import { TodoContext } from "../TodoContext";
 import { TodoCounter } from "../TodoCounter";
 import { TodoSearch } from "../TodoSearch";
 import { TodoList } from "../TodoList";
 import { TodoItem } from "../TodoItem";
 import { CreateTodoButton } from "../CreateTodoButton";
 
-function AppUI({
-  error,
-  loading,
-  completed,
-  total,
-  param,
-  setParam,
-  status,
-  setStatus,
-  listTodos,
-  onComplete,
-  onDelete
-}) {
+function AppUI() {
+  /* 
+            error={error}
+          loading={loading}
+          completed={completed.length}
+          total={todos.length}
+          param={search}
+          setParam={setSearch}
+          status={todos}
+          setStatus={saveTodos}
+          listTodos={listTodos}
+          onComplete={onCompleteTask}
+          onDelete={onDeleteTask}
+           */
 
   return (
     <React.Fragment>
-      <TodoCounter completed={completed.length} total={total.length} />
+      <TodoCounter />
 
-      <TodoSearch param={param} setParam={setParam} />
+      <TodoSearch />
 
-      <TodoList>
+      <TodoContext.Consumer>
+        {({
+          error,
+          loading,
+          listTodos,
+          todos,
+          saveTodos,
+          onCompleteTask,
+          onDeleteTask,
+        }) => {
+          return (
+            <TodoList>
+              {error && <p>Error cargando la informacion, preocupate</p>}
+              {loading && <p>Cargando informacion, dame un chance</p>}
+              {!loading && !listTodos.length && <p>Crea una nueva tarea</p>}
 
-        {error && <p>Error cargando la informacion, preocupate</p>}
-        {loading && <p>Cargando informacion, dame un chance</p>}
-        {(!loading && !listTodos.length) && <p>Crea una nueva tarea</p>}
-
-        {listTodos.length >= 1
-          ? listTodos.map((todo) => (
-              <TodoItem
-                key={todo.text}
-                text={todo.text}
-                status={status}
-                setStatus={setStatus}
-                completeTodo={() => onComplete(todo.text)}
-                deleteTodo={() => onDelete(todo.text)}
-              />
-            ))
-          : "No existe esta tarea"}
-      </TodoList>
+              {listTodos.map((todo) => (
+                <TodoItem
+                  key={todo.text}
+                  text={todo.text}
+                  status={todos}
+                  setStatus={saveTodos}
+                  completeTodo={() => onCompleteTask(todo.text)}
+                  deleteTodo={() => onDeleteTask(todo.text)}
+                />
+              ))}
+            </TodoList>
+          );
+        }}
+      </TodoContext.Consumer>
 
       <CreateTodoButton />
     </React.Fragment>
--- src/App/index.js
+++ src/App/index.js
@@ -2,106 +2,16 @@
 import React from "react";
 import { AppUI } from "./AppUI";
 import { Layout } from "../Layout";
-
-// Custom Hooks
-function useLocalStorage(itemName, initialValue) {
-  const [error, setError] = React.useState();
-  const [loading, setLoading] = React.useState();
-  const [item, setItem] = React.useState(initialValue);
-  
- // Uso de useEffect()
-  React.useEffect(() => {
-
-    setTimeout(() => {
-      try {
-        const localStorageTodos = localStorage.getItem(itemName);
-        let parsedTodos;
-
-        // Se verifica si ya existe informacion almacenada, sino se crea.
-        if (!localStorageTodos) {
-          localStorage.setItem(itemName, JSON.stringify(initialValue));
-          parsedTodos = [];
-        } else {
-          parsedTodos = JSON.parse(localStorageTodos);
-        }
-
-        setItem(parsedTodos);
-        setLoading(false);
-      } catch (error) {
-        setError(true);
-      }
-    }, 1000);
-  });
-
-  const saveTodos = (newArr) => {
-    localStorage.setItem(itemName, JSON.stringify(newArr));
-    setItem(newArr);
-  };
-
-  return { item, saveTodos, loading, error };
-}
+import { TodoProvider } from "../TodoContext";
 
 function App() {
-  const {
-    item: todos,
-    saveItem: saveTodos,
-    loading,
-    error,
-  } = useLocalStorage("TODOS_V1", []);
-
-  const [search, setSearch] = React.useState("");
-
-  let completed = todos.filter((index) => index.completed === true);
-
-  let listTodos = [];
-
-  if (search.length >= 1) {
-    listTodos = todos.filter((index) => {
-      const txt = index.text;
-      const txtLower = txt.toLowerCase();
-      return txtLower.includes(search);
-    });
-  } else {
-    listTodos = todos;
-  }
-
-  /* Si una funcion requiere de un parametro que se emplea en el Html, es necesario usar la arrow function */
-  const onCompleteTask = (txt) => {
-    let listAux = todos;
-    listAux = listAux.filter((index) => {
-      if (index.text === txt) {
-        index.completed = !index.completed;
-        // console.log(`Listo ${index.text}`);
-      }
-      return index;
-    });
-    saveTodos(listAux);
-  };
-
-  const onDeleteTask = (txt) => {
-    let i = todos.findIndex((index) => index.text === txt);
-    let listNew = [...todos];
-    listNew.splice(i, 1);
-    saveTodos(listNew);
-  };
-
   return (
     <React.Fragment>
-      <Layout>
-        <AppUI
-          error={error}
-          loading={loading}
-          completed={completed.length}
-          total={todos.length}
-          param={search}
-          setParam={setSearch}
-          status={todos}
-          setStatus={saveTodos}
-          listTodos={listTodos}
-          onComplete={onCompleteTask}
-          onDelete={onDeleteTask}
-        />
-      </Layout>
+      <TodoProvider>
+        <Layout>
+          <AppUI />
+        </Layout>
+      </TodoProvider>
     </React.Fragment>
   );
 }
