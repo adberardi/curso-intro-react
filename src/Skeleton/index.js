468ab5dc30716dd8307b8af186d36036949bbc34
Antonio Berardi <adberardi@yahoo.com>
2022-02-01 20:27:17 -0400

    Probando: Tu propio loading skeleton en React

--- src/Skeleton/index.js
+++ src/Skeleton/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import ContentLoader from "react-content-loader";
+// import ContentLoader from "react-content-loader";
+import "./skeleton.css";
 
 const Loading = () => (
-  <div className="TodoItem">
+  /*   <div className="TodoItem">
     <ContentLoader
       speed={2}
       width={480}
@@ -14,6 +15,12 @@ const Loading = () => (
       <rect x="10" y="12" width="12" height="12" />
       <rect x="25" y="15" rx="5" ry="5" width="220" height="10" />
     </ContentLoader>
+  </div> */
+
+  <div className="loading-container">
+    <span className="loading-checkbox"></span>
+    <p className="loading-text">Cargando informacion, espere.....</p>
+    <span className="loading-btn"></span>
   </div>
 );
 
